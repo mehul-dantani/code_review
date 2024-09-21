af78f7ebcf152b1cb726cf71ee37f97e9d276796
mehul-dantani <mehul@dxfactor.com>
2024-09-21 11:53:12 +0530

    updated code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
-ap.user(express.json)
+ap.user(express.json
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
