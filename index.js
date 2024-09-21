e8c9ead453ab4c59bb34853b6a28eed44f0fcfcf
mehul-dantani <mehul@dxfactor.com>
2024-09-21 13:43:05 +0530

    opimized code

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 // index.js
 
 const express = require('express');
-const app = express();
 
-// Define a port
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, Node!');
-});
+app.get('/', (req, res) => res.send('Hello, Node!'));
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
